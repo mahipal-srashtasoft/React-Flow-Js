97c5e2cc173eb20c167856eecf9d1486e6c89620
Mahipal Srashtasoft <mahipal.srashtasoft@gmail.com>
2025-02-10 18:29:53 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { ReactFlow } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>App</h1>
+    </>
+  )
+}
+
+export default App
